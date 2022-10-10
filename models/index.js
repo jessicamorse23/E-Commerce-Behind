154df571d660a25c194dc55df48915887cc87f7d
[Jessica Morse] <[jessicabrookemorse@gmail.com]>
2022-10-10 10:51:43 -0400

    added code to index.js in models folder

--- models/index.js
+++ models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "categoryId"
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "categoryId"
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "productId"
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag, 
+  foreignKey: "tagId"
+})
 
 module.exports = {
   Product,
@@ -18,3 +32,5 @@ module.exports = {
   Tag,
   ProductTag,
 };
+
+// I found the code for this model index.js in module 13.23 - one-to-many
